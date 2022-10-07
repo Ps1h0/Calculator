8b4ecc6bf26b6f2468b338d81bcb78540366bfd6
Ps1h0 <platonov.liceum@mail.ru>
2022-10-08 02:12:06 +0300

    Правка для читаемости

--- src/main/java/validators/BracketsValidator.java
+++ src/main/java/validators/BracketsValidator.java
@@ -2,26 +2,22 @@ package validators;
 
 import java.util.Stack;
 
-public class BracketsValidator implements Validator{
+public class BracketsValidator implements Validator {
 
     @Override
     public boolean isValid(String expression) {
-            Stack<Character> stack = new Stack<>();
-            for (char current : expression.toCharArray()) {
-                if (current == '(') {
-                    stack.push(current);
-                    continue;
-                }
-                if (current == ')')
-                    if (!stack.isEmpty()) {
-                        char fromStack = stack.peek();
-                        if (fromStack == '(')
-                            stack.pop();
-                        else
-                            return false;
-                    } else
-                        return false;
+        Stack<Character> stack = new Stack<>();
+        for (char current : expression.toCharArray()) {
+            if (current == '(') {
+                stack.push(current);
+                continue;
             }
-            return stack.isEmpty();
+            if (current == ')') if (!stack.isEmpty()) {
+                char fromStack = stack.peek();
+                if (fromStack == '(') stack.pop();
+                else return false;
+            } else return false;
+        }
+        return stack.isEmpty();
     }
 }

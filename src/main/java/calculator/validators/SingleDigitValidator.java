1fa58a9daa050cd8bcebeddfa9a40752f639fade
Ps1h0 <platonov.liceum@mail.ru>
2022-10-08 17:29:13 +0300

    Убран Prioritizer Проверка через Classifier

--- src/main/java/calculator/validators/SingleDigitValidator.java
+++ src/main/java/calculator/validators/SingleDigitValidator.java
@@ -1,20 +1,14 @@
-package validators;
+package calculator.validators;
 
-import util.Prioritizer;
+import calculator.util.Classifier;
 
 public class SingleDigitValidator implements Validator {
-
-    private final Prioritizer prioritizer;
-
-    {
-        prioritizer = new Prioritizer();
-    }
 
     @Override
     public boolean isValid(String expression) {
         if (expression.trim().length() == 1) {
             char symbol = expression.toCharArray()[0];
-            return prioritizer.getPriority(symbol) == 0;
+            return Classifier.isDigit(symbol);
         }
         return true;
     }

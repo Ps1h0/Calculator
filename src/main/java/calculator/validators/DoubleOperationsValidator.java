5c5abef73162b92c231e075dd582e2c09f2f4d49
Ps1h0 <platonov.liceum@mail.ru>
2022-10-08 17:26:17 +0300

    Убран Prioritizer Проверка символов изменена через Classifier

--- src/main/java/calculator/validators/DoubleOperationsValidator.java
+++ src/main/java/calculator/validators/DoubleOperationsValidator.java
@@ -1,19 +1,13 @@
-package validators;
+package calculator.validators;
 
-import util.Prioritizer;
+import calculator.util.Classifier;
 
 public class DoubleOperationsValidator implements Validator {
-
-    private final Prioritizer prioritizer;
-
-    {
-        prioritizer = new Prioritizer();
-    }
 
     @Override
     public boolean isValid(String expression) {
         for (int i = 0; i < expression.length() - 1; i++) {
-            if (prioritizer.getPriority(expression.charAt(i)) > 1 && prioritizer.getPriority(expression.charAt(i + 1)) > 1) {
+            if (Classifier.isOperation(expression.charAt(i)) && Classifier.isOperation(expression.charAt(i + 1))) {
                 return false;
             }
         }
